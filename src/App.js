216470bd769d30a7f554b0d24768876e9c99752e
Gibbsface <cole.gibbs@gmail.com>
2019-06-04 16:52:28 -0400

    Added flex-box functionality to the navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./Components/NavBar/NavBar";
-import styled from "@emotion/styled";
-
-const PageContainer = styled.body`
-  width: 100%;
-  height: 100%;
-  background: #ffffff;
-`;
+//import styled from "@emotion/styled";
 
 class App extends Component {
-  // changeLightStatus = () => {
-  //   this.setState({
-  //     lightStatus: !this.state.lightStatus
-  //   });
-  // };
-
   render() {
-    //const { lightStatus } = this.state;
-
-    return (
-      <PageContainer>
-        <Navbar />
-      </PageContainer>
-    );
+    return <Navbar />;
   }
 }
 
