34df510a3ac6d0ab294e660d8e2c1375c509f179
Gibbsface <cole.gibbs@gmail.com>
2019-06-06 16:20:40 -0400

    Huge update, added routing to all the different pages on the NavBar

---
+++ src/Components/Pages/Kingdom/KingdomPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class KingdomPage extends Component {
+  render() {
+    return <div>Hello World! Welcome to the Kingdom page</div>;
+  }
+}
+
+export default KingdomPage;
