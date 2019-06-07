ea3b588411ebdbdf3f806fa6e82ccf74df042f80
Jizhou Zhang <Jzhang3@oakland.edu>
2019-06-07 11:35:01 -0400

    About Page kinda

--- src/Components/Pages/About/AboutPage.js
+++ src/Components/Pages/About/AboutPage.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import Navbar from "../../NavBar/NavBar";
 import Footer from "../../Footer/Footer";
-
+import styled from "@emotion/styled";
+import map from "react-map-gl";
+const Info = styled.h1`
+  font-size: 30 px;
+  opacity: 3;
+  width: 70%;
+  text-align: center;
+`;
 class AboutPage extends Component {
   render() {
     return (
       <>
         <Navbar />
+        <Info>
+          In this page, you will learn all about our interns/missionaries for
+          the summer at Cru HQ.
+        </Info>
         <Footer />
       </>
     );
---
+++ src/Components/Pages/About/WorldMap.js
@@ -0,0 +1,8 @@
+import React, { PureComponent } from "react";
+import MapGl, { ScatterplotOverlay } from "react-map-gl";
+import styled from "@emotion/styled";
+class ReactMapGl extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+}
