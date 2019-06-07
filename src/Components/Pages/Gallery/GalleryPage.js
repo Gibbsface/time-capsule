6273d1d8b4476c7662ede18e5f29eead7c7cc46a
abigordon04 <abigailgordon04@gmail.com>
2019-06-07 10:55:27 -0400

    added gallery component

--- src/Components/Pages/Gallery/GalleryPage.js
+++ src/Components/Pages/Gallery/GalleryPage.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Navbar from "../../NavBar/NavBar";
 import Footer from "../../Footer/Footer";
+import Gallery from "react-grid-gallery";
+import IMAGES from "./Images";
 
 class GalleryPage extends Component {
   render() {
     return (
       <>
         <Navbar />
+        <Gallery images={IMAGES} />
         <Footer />
       </>
     );
---
+++ src/Components/Pages/Gallery/Images.js
@@ -0,0 +1,14 @@
+import Sunrise from "../../../Images/Sunrise.jpeg";
+
+const IMAGES = [
+  {
+    src: Sunrise,
+    thumbnail: Sunrise,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "Psalm 65:8"
+  }
+];
+
+export default IMAGES;
