f65dec782a9ffb434d8bfb02a52e24f5d7b8adb8
Jizhou Zhang <Jzhang3@oakland.edu>
2019-06-07 14:31:47 -0400

    Added pins onto map

--- src/Components/Pages/About/AboutPage.js
+++ src/Components/Pages/About/AboutPage.js
@@ -2,13 +2,46 @@ import React, { Component } from "react";
 import Navbar from "../../NavBar/NavBar";
 import Footer from "../../Footer/Footer";
 import styled from "@emotion/styled";
+import ReactMapGL, { Marker, Popup, NavigationControl } from "react-map-gl";
+import TOKEN from "../../../config/tokens.json";
+import "mapbox-gl/dist/mapbox-gl.css";
+import LocationPin from "../../Locations/LocationPins";
+import LOCATIONS from "../../Locations/locations.json";
 const Info = styled.h1`
-  font-size: 30 px;
+  font-size: 20 px;
   opacity: 3;
-  width: 70%;
+  width: 100%;
   text-align: center;
+  text-size: 20 px;
 `;
 class AboutPage extends Component {
+  state = {
+    viewport: {
+      width: "100%",
+      height: 400,
+      latitude: 39.381266,
+      longitude: -97.922211,
+      zoom: 3
+    },
+    popupInfo: null
+  };
+  componentDidMount() {
+    console.log(TOKEN.MAPBOX_ACCESS_TOKEN);
+  }
+  _renderLocationMarker = (location, index) => {
+    return (
+      <Marker
+        key={`marker-${index}`}
+        longitude={location.longitude}
+        latitude={location.latitude}
+      >
+        <LocationPin
+          size={20}
+          onClick={() => this.setState({ popupinfo: location })}
+        />
+      </Marker>
+    );
+  };
   render() {
     return (
       <>
@@ -17,6 +50,13 @@ class AboutPage extends Component {
           In this page, you will learn all about our interns/missionaries for
           the summer at Cru HQ.
         </Info>
+        <ReactMapGL
+          mapboxApiAccessToken={TOKEN.MAPBOX_ACCESS_TOKEN}
+          {...this.state.viewport}
+          onViewportChange={viewport => this.setState({ viewport })}
+        >
+          {LOCATIONS.map(this._renderLocationMarker)}
+        </ReactMapGL>
         <Footer />
       </>
     );
--- src/Components/Pages/About/WorldMap.js
+++
@@ -1,8 +0,0 @@
-import React, { PureComponent } from "react";
-import MapGl, { ScatterplotOverlay } from "react-map-gl";
-import styled from "@emotion/styled";
-class ReactMapGl extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-}
