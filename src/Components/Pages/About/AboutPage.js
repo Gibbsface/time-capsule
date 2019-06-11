8b66aedabd792eef0beda0abfa8c63b4d5b4142d
Jizhou Zhang <Jzhang3@oakland.edu>
2019-06-11 16:52:07 -0400

    Made some test on the about pages

--- src/Components/Pages/About/AboutPage.js
+++ src/Components/Pages/About/AboutPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
 import Navbar from "../../NavBar/NavBar";
 import Footer from "../../Footer/Footer";
 import ReactMapGL, { Marker } from "react-map-gl";
@@ -8,12 +7,22 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import LocationPin from "../../Locations/LocationPins";
 import LOCATIONS from "../../Locations/locations.json";
 import MAP_STYLE from "../../../config/map_config.json";
-import INTERNS from "../../Interns/Profile";
 import styled from "@emotion/styled";
-const Info = styled.h1`
+import Jizhou_Image from "../../../Images/InternProfile/me.jpg";
+import { red } from "ansi-colors";
+const Jizhou = styled.div`
+  background-image: url(${Jizhou_Image});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 250px;
+  width: 250px;
+`;
+const Info = styled.h3`
   font-size: 20 px;
   opacity: 3;
   width: 100%;
+  background-color: orange;
   text-align: center;
   text-size: 20 px;
 `;
@@ -25,16 +34,13 @@ const Intern = styled.h2`
 const Bio = styled.p`
   transform: [{ rotate: '90deg'}];
 `;
-
 const GridContainer = styled.div`
   width: 86%;
+  background-color: orange;
   margin: 0 auto;
   display: grid;
-
   padding: 1rem;
-
   grid-template-columns: repeat(3, 1fr);
-
   grid-row-gap: 1rem;
 `;
 class AboutPage extends Component {
@@ -68,7 +74,9 @@ class AboutPage extends Component {
         <Navbar />
         <Info>
           In this page, you will learn all about our interns/missionaries for
-          the summer at Cru HQ.
+          the summer at Cru HQ Summer Mission. This page includes information on
+          where they come from, what department they work in at HQ, and other
+          biographic information
         </Info>
         <ReactMapGL
           mapStyle={MAP_STYLE.MAP_STYLE_LINK}
@@ -80,54 +88,78 @@ class AboutPage extends Component {
         </ReactMapGL>
         <GridContainer>
           <div>
+            <Jizhou />
             <Intern>Jizhou</Intern>
             <Bio>
-              This is my project. Testing Testing Testing Testing Testing
+              Jizhou is an upcoming Senior studying Computer Science at Oakland
+              University. He works in the DPS department.
             </Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Cole</Intern>
             <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Callie</Intern> <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Emily</Intern> <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>David</Intern> <Bio>txt</Bio>
           </div>
           <div>
             {" "}
+            <Jizhou />
             <Intern>Kellechi</Intern> <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Justin</Intern> <Bio>txt</Bio>
           </div>
           <div>
             {" "}
+            <Jizhou />
             <Intern>Abi</Intern>
             <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Laci</Intern>
             <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Danny</Intern>
             <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Brittany</Intern>
             <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Johnny</Intern>
             <Bio>txt</Bio>
           </div>
           <div>
+            <Jizhou />
             <Intern>Donovan</Intern>
+            <Bio>txt</Bio>
+          </div>
+          <div>
+            <Jizhou />
+            <Intern>Jessica</Intern>
+            <Bio>txt</Bio>
+          </div>
+          <div>
+            <Jizhou />
+            <Intern>Chun</Intern>
             <Bio>txt</Bio>
           </div>
         </GridContainer>
