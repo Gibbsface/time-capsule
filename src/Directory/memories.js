f8e54f46c1bd48d3a45c50f86b6afdb1c70a38e1
Gibbsface <cole.gibbs@gmail.com>
2019-06-17 09:32:09 -0400

    Added image array for first memory

--- src/Directory/memories.js
+++ src/Directory/memories.js
@@ -1,30 +1,200 @@
 import data from "./memories.json";
 
-import AppleCrisp from "../Images/Fun/IMG_5508.JPG";
-import Worship0 from "../Images/Fun/IMG_5510.JPG";
+import CBeach0 from "../Images/BeachOutreach/6.1.19/Beach0.jpg";
+import CBeach1 from "../Images/BeachOutreach/6.1.19/Beach1.jpg";
+import CBeach2 from "../Images/BeachOutreach/6.1.19/Beach2.jpg";
+import CBeach3 from "../Images/BeachOutreach/6.1.19/Beach3.jpg";
+import CBeach4 from "../Images/BeachOutreach/6.1.19/Beach4.jpg";
+import CBeach5 from "../Images/BeachOutreach/6.1.19/Beach5.jpg";
+import CBeachTraining0 from "../Images/BeachOutreach/6.1.19/Training0.jpg";
+import CBeachTraining1 from "../Images/BeachOutreach/6.1.19/Training1.jpg";
+import CBeachTraining2 from "../Images/BeachOutreach/6.1.19/Training2.jpg";
+import CBeachTraining3 from "../Images/BeachOutreach/6.1.19/Training3.jpg";
+import CBeachTraining4 from "../Images/BeachOutreach/6.1.19/Training4.jpg";
+import CBeachTraining5 from "../Images/BeachOutreach/6.1.19/Training5.jpg";
+import CBeachTraining6 from "../Images/BeachOutreach/6.1.19/Training6.jpg";
+import CBeachTraining7 from "../Images/BeachOutreach/6.1.19/Training7.jpg";
+import CBeachTraining8 from "../Images/BeachOutreach/6.1.19/Training8.jpg";
+import CBeachTraining9 from "../Images/BeachOutreach/6.1.19/Training9.jpg";
+import CBeachTraining10 from "../Images/BeachOutreach/6.1.19/Training10.jpg";
+import CBeachTraining11 from "../Images/BeachOutreach/6.1.19/Training11.jpg";
+import CBeachTraining12 from "../Images/BeachOutreach/6.1.19/Training12.jpg";
+import CBeachTraining13 from "../Images/BeachOutreach/6.1.19/Training13.jpg";
+import CBeachTraining14 from "../Images/BeachOutreach/6.1.19/Training14.jpg";
 
 const Memories = data.Memories;
 
-const Scavenger = [
+const CBeachOutreachPhotos = [
   {
-    src: AppleCrisp,
-    thumbnail: AppleCrisp,
+    src: CBeach0,
+    thumbnail: CBeach0,
     thumbnailWidth: 320,
     thumbnailHeight: 174,
     isSelected: false,
-    caption:
-      "Everyone joins together to play some cards and eat some Apple Crisp"
+    caption: "test test test"
   },
   {
-    src: Worship0,
-    thumbnail: Worship0,
+    src: CBeach1,
+    thumbnail: CBeach1,
     thumbnailWidth: 320,
     thumbnailHeight: 174,
     isSelected: false,
-    caption: "Gathering together for some praise and worship"
+    caption: "test test test"
+  },
+  {
+    src: CBeach2,
+    thumbnail: CBeach2,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeach3,
+    thumbnail: CBeach3,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeach4,
+    thumbnail: CBeach4,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeach5,
+    thumbnail: CBeach5,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining0,
+    thumbnail: CBeachTraining0,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining1,
+    thumbnail: CBeachTraining1,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining2,
+    thumbnail: CBeachTraining2,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining3,
+    thumbnail: CBeachTraining3,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining4,
+    thumbnail: CBeachTraining4,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining5,
+    thumbnail: CBeachTraining5,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining6,
+    thumbnail: CBeachTraining6,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining7,
+    thumbnail: CBeachTraining7,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining8,
+    thumbnail: CBeachTraining8,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining9,
+    thumbnail: CBeachTraining9,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining10,
+    thumbnail: CBeachTraining10,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining11,
+    thumbnail: CBeachTraining11,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining12,
+    thumbnail: CBeachTraining12,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining13,
+    thumbnail: CBeachTraining13,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
+  },
+  {
+    src: CBeachTraining14,
+    thumbnail: CBeachTraining14,
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: false,
+    caption: "test test test"
   }
 ];
 
-Memories[0].photos = Scavenger;
+Memories[0].photos = CBeachOutreachPhotos;
 
 export default Memories;
