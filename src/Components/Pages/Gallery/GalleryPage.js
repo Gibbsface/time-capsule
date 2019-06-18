8eb7d5b87129e7dc52063a1d3bd65f2ed7bf88b2
Gibbsface <cole.gibbs@gmail.com>
2019-06-18 11:59:24 -0400

    removed gallery for now, making buttons maybe... we'll see after lunch

--- src/Components/Pages/Gallery/GalleryPage.js
+++ src/Components/Pages/Gallery/GalleryPage.js
@@ -6,25 +6,16 @@ import Gallery from "react-grid-gallery";
 import Memories from "../../../Directory/memories.js";
 import _ from "lodash";
 
-const Titles = styled.div`
-  background-color: transparent;
-  color: white;
-  font-size: 30px;
-  padding: 10px;
-  text-decoration: none;
-`;
-
 const PageContainer = styled.div`
-  background-color: #666062;
-  position: 0 0;
-  margin: auto;
   padding-right: 7%;
   padding-left: 7%;
   width: 86%;
   padding-top: 15px;
+`;
 
-  display: flex;
-  flex-direction: column;
+const AlbumContainer = styled.img`
+  background-color: red;
+  width: 100%;
 `;
 
 //TODO: use lodash to map through all memories to display pictures,
@@ -38,17 +29,11 @@ class GalleryPage extends Component {
         <PageContainer>
           {_.map(Memories, Story => {
             return Story.photos != null ? (
-              <section-container key={"gallery_" + Story.id}>
-                <Titles key={"title_" + Story.id}>
-                  {"\n"} {Story.title} {"\n"}
-                </Titles>
-                <Gallery
-                  key={"images_" + Story.id}
-                  images={Story.photos}
-                  enableImageSelection={false}
-                  maxRows={1}
-                />
-              </section-container>
+              <AlbumContainer
+                src={Story.photos[0].src}
+                alt='test test'
+                key={"album" + Story.id}
+              />
             ) : null;
           })}
         </PageContainer>
@@ -59,3 +44,20 @@ class GalleryPage extends Component {
 }
 
 export default GalleryPage;
+
+// {_.map(Memories, Story => {
+//   return Story.photos != null ? (
+//     <section-container key={"gallery_" + Story.id}>
+//       <Titles key={"title_" + Story.id}>
+//         {"\n"} {Story.title} {"\n"}
+//       </Titles>
+//       <Gallery
+//         key={"images_" + Story.id}
+//         images={Story.photos}
+//         enableImageSelection={false}
+//         maxRows={1}
+//         backdropClosesModal={true}
+//       />
+//     </section-container>
+//   ) : null;
+// })}
