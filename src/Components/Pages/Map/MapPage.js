efcc7753c6e1b2e41a64fc3c07b81cf5b9361f03
jammerwhy <david.yue@cru.org>
2019-06-24 14:19:39 -0400

    Added memory card component

--- src/Components/Pages/Map/MapPage.js
+++ src/Components/Pages/Map/MapPage.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
 import styled from "@emotion/styled";
+import _ from "lodash";
 
 import ReactMapGL from "react-map-gl";
 import TOKEN from "../../../config/tokens.json";
 import MAP_STYLE from "../../../config/map_config.json";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+import Memories from "../../../Directory/memories.js";
+
 import Navbar from "../../NavBar/NavBar";
 import Footer from "../../Footer/Footer";
-
-const MapContainer = styled.div`
-  padding-right: 7%;
-  padding-left: 7%;
-  width: 86%;
-`;
+import MemoryCard from "./Subcomponents/MemoryCard";
 
 class MapPage extends Component {
   state = {
@@ -30,14 +28,21 @@ class MapPage extends Component {
     return (
       <>
         <Navbar />
-        <MapContainer>
-          <ReactMapGL
-            mapStyle={MAP_STYLE.MAP_STYLE_LINK}
-            mapboxApiAccessToken={TOKEN.MAPBOX_ACCESS_TOKEN}
-            {...this.state.viewport}
-            onViewportChange={viewport => this.setState({ viewport })}
-          />
-        </MapContainer>
+        <PageContainer>
+          <MapContainer>
+            <ReactMapGL
+              mapStyle={MAP_STYLE.MAP_STYLE_LINK}
+              mapboxApiAccessToken={TOKEN.MAPBOX_ACCESS_TOKEN}
+              {...this.state.viewport}
+              onViewportChange={viewport => this.setState({ viewport })}
+            />
+            <CardContainer>
+              {_.map(Memories, memory => {
+                return <MemoryCard cardData={memory} key={memory.id} />;
+              })}
+            </CardContainer>
+          </MapContainer>
+        </PageContainer>
         <Footer />
       </>
     );
@@ -45,3 +50,31 @@ class MapPage extends Component {
 }
 
 export default MapPage;
+
+const PageContainer = styled.div`
+  background-color: #666062;
+  position: 0 0;
+  margin: auto;
+  padding-right: 7%;
+  padding-left: 7%;
+  width: 86%;
+  padding-top: 15px;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const MapContainer = styled.div`
+  padding-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 700px;
+  overflow: auto;
+  width: 40%;
+  margin-left: 10px;
+`;
---
+++ src/Components/Pages/Map/Subcomponents/MemoryCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const MemoryCard = ({ cardData }) => {
+  return (
+    <CardContainer>
+      <Title>{cardData.title}</Title>
+      <Story>{cardData.story}</Story>
+    </CardContainer>
+  );
+};
+
+export default MemoryCard;
+
+const CardContainer = styled.div`
+  padding: 20px;
+  border: 1px solid #eee;
+  border-left-width: 7px;
+  border-radius: 3px;
+  border-left-color: #3eb1c8;
+  width: auto;
+  display: inline-block;
+  vertical-align: top;
+  margin-bottom: 10px;
+  background-color: #ffffff;
+`;
+
+const Title = styled.p`
+  font-size: 24px;
+  margin: 5px 0px;
+  color: #333333;
+`;
+
+const Story = styled.p`
+  margin-top: 0px;
+`;
